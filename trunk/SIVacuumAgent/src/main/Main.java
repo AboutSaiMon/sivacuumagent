1864ac43a54c32f71efa5d91d76e1c140a571ab9
carminedodaro <carminedodaro@9cdeb93a-9422-2c83-24a3-7f0976b358d8>
2010-11-19 22:51:52 +0000

    Aggiornato il funzionamento!

--- trunk/SIVacuumAgent/src/main/Main.java
+++ trunk/SIVacuumAgent/src/main/Main.java
@@ -27,7 +27,7 @@ public class Main extends JFrame{
 	JMenuItem generateMap = new JMenuItem( Constants.GENERATEMAP );
 	
 	VAFloor floor = new VAFloor( 100 );
-	Point point = new Point( 2, 0 );
+	Point point = new Point( 0, 0 );
 	
 	public Main() {
 		// TODO Auto-generated constructor stub
@@ -69,7 +69,7 @@ public class Main extends JFrame{
 		return load;
 	}
 
-	public void setLoad(JMenuItem load) {
+	public void setLoad( JMenuItem load ) {
 		this.load = load;
 	}
 
@@ -77,7 +77,7 @@ public class Main extends JFrame{
 		return save;
 	}
 
-	public void setSave(JMenuItem save) {
+	public void setSave( JMenuItem save ) {
 		this.save = save;
 	}
 
@@ -101,7 +101,7 @@ public class Main extends JFrame{
 		return floor;
 	}
 
-	public void setFloor(VAFloor floor) {
+	public void setFloor( VAFloor floor ) {
 		this.floor = floor;
 	}
 
@@ -109,7 +109,7 @@ public class Main extends JFrame{
 		return point;
 	}
 
-	public void setPoint(Point point) {
+	public void setPoint( Point point ) {
 		this.point = point;
 	}
 
--- trunk/SIVacuumAgent/src/view/principal/GenerateMap.java
+++ trunk/SIVacuumAgent/src/view/principal/GenerateMap.java
@@ -1,36 +1,97 @@
 package view.principal;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import exception.NegativeNumberException;
+
 import main.Main;
+import util.constants.Constants;
 import vacuumAgent.environment.VAEnvObservable;
 import view.EnvironmentDrawPanel.FloorPanel;
 
 public class GenerateMap implements ActionListener {
 
-	Main frame;
+	Main principalFrame;
 		
-	public GenerateMap( Main frame ) 
+	public GenerateMap( Main principalFrame ) 
 	{
 		super();
-		this.frame = frame;		
+		this.principalFrame = principalFrame;		
 	}
 
 
 	@Override 
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		FloorPanel floorPanel;
-		VAEnvObservable state = new VAEnvObservable( null, frame.getPoint(), frame.getFloor() );				
-		floorPanel = new FloorPanel( state );
-		floorPanel.setEditable( true );
-		frame.setSize( 800, 600 );
-		frame.setContentPane( floorPanel );
-		frame.getGenerateMap().setEnabled( false );
-		frame.getGenerateRandomly().setEnabled( false );
-		frame.getLoad().setEnabled( false );
-		frame.getSave().setEnabled( true );
-		frame.getSave().addActionListener( new SaveFileChooser( frame ) );
+		final JFrame frame = new JFrame( Constants.TITLE );
+		frame.setResizable( false);
+		frame.setVisible( true );
+		frame.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+		frame.setSize( 230, 150 );
+		frame.setLocation( 300, 300 );
+		
+		JPanel panel = new JPanel();
+		
+		panel.setLayout( new GridLayout( 2, 2 ) );			
+		
+		JLabel tailsNumberLabel = new JLabel( Constants.TAILSNUMBER );
+		panel.add( tailsNumberLabel );
+	
+		final JTextField tailsNumber = new JTextField();
+		panel.add( tailsNumber );
+		
+		JButton generate = new JButton( Constants.GENERATE );
+		
+		generate.addActionListener( new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {						
+				// TODO Auto-generated method stub
+				String tailsNumberText = tailsNumber.getText();				
+				
+				try
+				{
+					int tails = Integer.parseInt( tailsNumberText );
+					
+					if( tails <= 0 )
+						throw new NegativeNumberException( Constants.ERRORNEGATIVENUMBERS);
+					principalFrame.getFloor().setSize( tails );
+					FloorPanel floorPanel;
+					
+					VAEnvObservable state = new VAEnvObservable( null, principalFrame.getPoint(), principalFrame.getFloor() );				
+					floorPanel = new FloorPanel( state );
+					floorPanel.setEditable( true );
+					principalFrame.setSize( 800, 600 );
+					principalFrame.setContentPane( floorPanel );
+					principalFrame.getGenerateMap().setEnabled( false );
+					principalFrame.getGenerateRandomly().setEnabled( false );
+					principalFrame.getLoad().setEnabled( false );
+					principalFrame.getSave().setEnabled( true );
+					principalFrame.getSave().addActionListener( new SaveFileChooser( principalFrame ) );
+					frame.dispose();
+				}
+				catch( NumberFormatException ex )
+				{							
+					JOptionPane.showMessageDialog( null, Constants.ERRORONNUMBER, Constants.ERROR, JOptionPane.ERROR_MESSAGE );
+				} catch ( NegativeNumberException e ) {
+					// TODO Auto-generated catch block
+					JOptionPane.showMessageDialog( null, Constants.ERRORNEGATIVENUMBERS, Constants.ERROR, JOptionPane.ERROR_MESSAGE );
+				}
+				
+			}
+		});
+		
+		panel.add( generate );	
+		
+		frame.setContentPane( panel );	
 	}
 }
