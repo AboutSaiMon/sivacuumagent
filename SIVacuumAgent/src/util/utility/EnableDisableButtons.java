9e19743891d44f41d0d7a682c0ff0c424ee204e8
carminedodaro <carminedodaro@9cdeb93a-9422-2c83-24a3-7f0976b358d8>
2010-12-18 08:58:40 +0000

    Rimosso Tasto Back. ( Da rifare )

--- SIVacuumAgent/src/util/utility/EnableDisableButtons.java
+++ SIVacuumAgent/src/util/utility/EnableDisableButtons.java
@@ -1,8 +1,5 @@
 package util.utility;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import main.Main;
 import view.principal.SaveFileChooserActionListener;
 import view.principal.StartActionListener;
@@ -24,17 +21,6 @@ public class EnableDisableButtons {
 		
 		principalFrame.getMoveOneStep().setEnabled( true );
 		principalFrame.getMoveOneStep().addActionListener( new StartActionListener( principalFrame, 1 ) );
-		
-		principalFrame.getBack().setEnabled( true );
-		principalFrame.getBack().addActionListener( new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				// TODO Auto-generated method stub
-				new Main( principalFrame.getEnvironment() );
-				principalFrame.dispose();
-			}
-		});
 		
 		principalFrame.getDefineAgent().setEnabled( false );
 		principalFrame.setResizable( true );
